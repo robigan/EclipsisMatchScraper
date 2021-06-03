8a4c54fe521956c634c1855fc5e6d0f69f294c46
robigan <35210888+robigan@users.noreply.github.com>
2021-06-03 22:51:58 +0200

    last night

--- src/dump.js
+++ src/dump.js
@@ -1,13 +1,13 @@
 const Config = require("../secret.hidden.json");
 const SnowTransfer = new (require("snowtransfer"))(Config.token, {});
 const limit = 100;
-const {promises: fs} = require("fs");
+const { promises: fs } = require("fs");
 
 /** @param {Array.<import("@amanda/discordtypings").MessageData>} Data */
 const ShaveData = async (Data) => {
     const Cache = [];
     for (const Message of Data) {
-        Cache.push({"id": Message.id, "embeds": Message.embeds, "timestamp": Message.timestamp});
+        Cache.push({ "id": Message.id, "embeds": Message.embeds, "timestamp": Message.timestamp });
     }
     return Cache;
 };
@@ -17,17 +17,15 @@ const ShaveData = async (Data) => {
     let Cache = null;
     let BeforeOffset;
     const File = await fs.open(`${(new Date).toISOString()}.txt`, "ax");
-    await File.appendFile("Start of messages dump\n");
-    Cache = await SnowTransfer.channel.getChannelMessages("530101234590547968", {"limit": limit});
+    Cache = await SnowTransfer.channel.getChannelMessages("530101234590547968", { "limit": limit });
     BeforeOffset = Cache[limit - 1].id;
     await File.appendFile(`${JSON.stringify(await ShaveData(Cache))}\n`);
     console.log(`Next snowflake: ${BeforeOffset}\nCount: ${Count}\n`);
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
+    setInterval(async () => {
         Count += 1;
-        Cache = await SnowTransfer.channel.getChannelMessages("530101234590547968", {"limit": limit, "before": BeforeOffset});
+        Cache = await SnowTransfer.channel.getChannelMessages("530101234590547968", { "limit": limit, "before": BeforeOffset });
         BeforeOffset = Cache[limit - 1].id;
         await File.appendFile(`${JSON.stringify(await ShaveData(Cache))}\n`);
         console.log(`Next snowflake: ${BeforeOffset}\nCount: ${Count}\n`);
-    }
+    }, 5000);
 })().catch(console.error);
