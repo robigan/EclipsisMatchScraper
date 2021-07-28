682b9c55e5e33805a15c9b1924acc257fb2b4381
robigan <35210888+robigan@users.noreply.github.com>
2021-07-28 18:01:32 +0200

    stats

--- raper.py
+++ raper.py
@@ -3,8 +3,9 @@ import json
 from pymongo import MongoClient
 import regex
 
+
 def main():
-    with open("/home/robigan/Documents/Source/EclipsisMatchScraper/R4p1ng Tools 2/secret.hidden.json") as json_file:
+    with open("/home/robigan/Documents/Source/EclipsisMatchScraper/secret.hidden.json") as json_file:
         config = json.load(json_file)
         json_file.close()
 
@@ -12,13 +13,30 @@ def main():
     #db = client['eclipsis-database']
     #col = db["matches"]
 
-    r = requests.get(
-        config["url"], params=config["getOptions"], headers=config["headers"])
-    #data = json.load(r.json())
+    r = requests.get(config["url"], params=config["getOptions"], headers=config["headers"])
+    data = r.json()
 
-    print(regex.findall("(?<=:arrow_down: |:arrow_down_small: |:trophy::arrow_up_small: |:trophy::arrow_up: |:arrow_double_down: |:trophy::arrow_double_up: ).*?(?= [[])", r.text))
-    print(regex.findall("(?<=\w [[]).*?(?=[]])", r.text))
-    print(regex.findall("(?<=]: ).*?(?= [-+])", r.text))
+    limit = config["getOptions"]["limit"]
+    success = 0
+
+    Usernames = []
+    Durations = []
+    Ratings = []
+
+    for Match in data:
+        UsernamesInMatch = regex.findall("(?<=:arrow_down: |:arrow_down_small: |:trophy::arrow_up_small: |:trophy::arrow_up: |#:arrow_double_down: |:trophy::arrow_double_up: ).*?(?= [[])", str(Match))
+        DurationInMatch = regex.findall("(?<=\w [[]).*?(?=[]])", str(Match))
+        RatingInMatch = regex.findall("(?<=]: ).*?(?= [-+])", str(Match))
+        if (len(UsernamesInMatch) == len(DurationInMatch) == len(RatingInMatch)):
+            success += 1
+            print(len(UsernamesInMatch), len(DurationInMatch), len(RatingInMatch), " Matched")
+        else:
+            print(len(UsernamesInMatch), len(DurationInMatch), len(RatingInMatch))
+
+    print("Parsing success rate stats: " + str((success / limit) * 100) + str("%"))
+    # Usernames = regex.findall("(?<=:arrow_down: |:arrow_down_small: |:trophy::arrow_up_small: |:trophy::arrow_up: |#:arrow_double_down: |:trophy::arrow_double_up: ).*?(?= [[])", r.text)
+    #Durations = regex.findall("(?<=\w [[]).*?(?=[]])", r.text)
+    #Ratings = regex.findall("(?<=]: ).*?(?= [-+])", r.text)
 
     #massWrite = []
 
