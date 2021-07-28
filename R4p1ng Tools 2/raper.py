763279e10f72b59ffa6dbc4d598d16e81c365eef
robigan <35210888+robigan@users.noreply.github.com>
2021-07-28 13:43:04 +0200

    OwO

--- R4p1ng Tools 2/raper.py
+++ R4p1ng Tools 2/raper.py
@@ -1,15 +1,18 @@
 import requests
 import json
+from pymongo import MongoClient
 
 def main():
-    with open("secret.hidden.json") as json_file:
-        data = json.load(json_file)
+    with open("/home/robigan/Documents/Source/EclipsisMatchScraper/R4p1ng Tools 2/secret.hidden.json") as json_file:
+        config = json.load(json_file)
         json_file.close()
 
-    r = requests.get(data.url, params=data.getOptions, headers=data.headers)
+    client = MongoClient(config["conn_url"], serverSelectionTimeoutMS=5000)
+    db = client['eclipsis-database']
+    col = db["matches"]
 
-    print(r.url)
-    print(r.text)
+    r = requests.get(config["url"], params=config["getOptions"], headers=config["headers"])
+
 
 if __name__ == "__main__":
     main()
--- R4p1ng Tools/index.js
+++
@@ -1,6 +0,0 @@
-const Config = require("./secret.hidden.json");
-const SnowTransfer = new (require("snowtransfer"))(Config.token, {});
-
-(async () => {
-    console.log(await SnowTransfer.channel.getChannelMessages("530101234590547968", {"limit": 100}));
-})().catch(console.error);
